429211ca8e5178a2e6c7a9e53f12dc4387364bdd
Atul <gupta.atul@indiacansolutions.com>
2017-07-13 12:57:39 +0530

    upgraded version

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
+function testing(){
+	console.log("testing");
+}
 module.exports = require("./build/");
