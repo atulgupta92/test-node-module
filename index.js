92c114a47674154fcfa3a967939740c5b6afab21
Atul <gupta.atul@indiacansolutions.com>
2017-07-14 19:20:26 +0530

    fixed the changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing v0.1.2");
+	console.log("testing v0.1.3");
 }
 module.exports = require("./build/");
