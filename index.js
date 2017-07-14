737ce763776aa70c9e1cd65802b15ecf7b28141b
Atul <gupta.atul@indiacansolutions.com>
2017-07-14 18:31:19 +0530

    re-initialize the package

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing v1.0.3");
+	console.log("testing v0.1.0");
 }
 module.exports = require("./build/");
