653116fd0e4efe99df073a704b7fbc20180d71c9
Atul <gupta.atul@indiacansolutions.com>
2017-09-01 15:47:23 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing npm update");
+	console.log("testing npm update1");
 }
 module.exports = require("./build/");
