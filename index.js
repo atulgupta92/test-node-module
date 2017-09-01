fd1ac8c16e9311f6666416a66f4cbb65f55c19e9
Atul <gupta.atul@indiacansolutions.com>
2017-09-01 15:54:41 +0530

    khelo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing npm chalo");
+	console.log("testing npm khelo");
 }
 module.exports = require("./build/");
