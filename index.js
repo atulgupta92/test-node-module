4800c4485b63aa20aa6e9384bdbd1181935b9a73
Atul <gupta.atul@indiacansolutions.com>
2017-09-01 18:13:25 +0530

    tele

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing npm chalao");
+	console.log("testing npm chalao tele");
 }
 module.exports = require("./build/");
