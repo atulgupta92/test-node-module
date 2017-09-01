c715caccd84f71562a7d1be8d032a451d0b430ca
Atul <gupta.atul@indiacansolutions.com>
2017-09-01 16:05:13 +0530

    udo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This is not the main file in the npm package, but here so we can use github
 // tarballs as packages when necessary.
 function testing(){
-	console.log("testing npm daudo");
+	console.log("testing npm udo");
 }
 module.exports = require("./build/");
